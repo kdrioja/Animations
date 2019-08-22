e7d11ea8596f2a2fa8e0ae5ea50d13c5a85994fd
Kenia Rioja <kdrioja@gmail.com>
2019-08-21 20:53:26 -0700

    Completed fade function

--- app/src/main/java/com/example/animations/MainActivity.java
+++ app/src/main/java/com/example/animations/MainActivity.java
@@ -3,8 +3,36 @@ package com.example.animations;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    boolean bartIsShowing = true;
+
+    public void fade(View view) {
+        Log.i("Info", "Bart image clicked.");
+
+        ImageView bartImageView = findViewById(R.id.bartImageView);
+        ImageView homerImageView = findViewById(R.id.homerImageView);
+
+        if (bartIsShowing) {
+            //fading out
+            bartImageView.animate().alpha(0).setDuration(2000);
+            //fading in
+            homerImageView.animate().alpha(1).setDuration(2000);
+
+            bartIsShowing = false;
+        }
+        else {
+            //fading out
+            homerImageView.animate().alpha(0).setDuration(2000);
+            //fading in
+            bartImageView.animate().alpha(1).setDuration(2000);
+
+            bartIsShowing = true;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
